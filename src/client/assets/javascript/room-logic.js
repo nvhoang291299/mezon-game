30af9a0342a3fec2807ab450a493b3b2da188274
nvhoang291299 <nvhoang020100@gmail.com>
2025-01-03 14:55:54 +0700

    update

--- src/client/assets/javascript/room-logic.js
+++ src/client/assets/javascript/room-logic.js
@@ -55,7 +55,7 @@ const renderListRoom = (listRooms) => {
               <div class="room-name">
                 <span class="room-name-text">${room?.roomInfo?.roomId}</span>
               </div>
-              <div class="room-players">${room?.roomInfo?.roomMembers?.length}/${room?.roomInfo?.roomMaxUser}</div>
+              <div class="room-players">${room?.roomInfo?.roomMaxUser}</div>
               <div class="room-join">
                 <button onclick="joinRoom('${room?.roomInfo?.roomId}')" class="room-join-btn">JOIN</button>
               </div>
